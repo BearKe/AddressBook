c91aa17e9261eac235e423b7df01ec4cee40a3e4
BearKe <247329052@qq.com>
2017-04-13 18:30:34 +0800

    Initial Commit

---
+++ AddressBook_e/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AddressBook_e
+//
+//  Created by KeX on 2017/4/13.
+//  Copyright © 2017年 KeX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
